ffbf0256dc53024b453ef34214513917da691c73
Leo Nam <myleobot@gmail.com>
2022-02-10 11:39:14 +0900

    page init

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,9 +1,83 @@
 import './sidebar.css'
+import { LineStyle, Timeline, TrendingUp, Person, Storefront, AttachMoney, Equalizer, MailOutline, DynamicFeed, ChatBubbleOutline, WorkOutline, ErrorOutline} from '@material-ui/icons'
 
 export default function Sidebar() {
   return (
 	<div className="sidebar">
-		sidebar
+		<div className="sidevarWrapper">
+			<div className="sidebarMenu">
+				<h3 className="sidebarTitle">Dashboard</h3>
+				<ul className="sidebarList">
+					<li className="sidebarListItem active">
+						<LineStyle className="sidebarIcon" />
+						Home
+					</li>
+					<li className="sidebarListItem">
+						<Timeline className="sidebarIcon" />
+						Analytics
+					</li>
+					<li className="sidebarListItem">
+						<TrendingUp className="sidebarIcon" />
+						Sales
+					</li>
+				</ul>
+			</div>
+			<div className="sidebarMenu">
+				<h3 className="sidebarTitle">Quick Menu</h3>
+				<ul className="sidebarList">
+					<li className="sidebarListItem">
+						<Person className="sidebarIcon" />
+						Users
+					</li>
+					<li className="sidebarListItem">
+						<Storefront className="sidebarIcon" />
+						Products
+					</li>
+					<li className="sidebarListItem">
+						<AttachMoney className="sidebarIcon" />
+						Transactions
+					</li>
+					<li className="sidebarListItem">
+						<Equalizer className="sidebarIcon" />
+						Reports
+					</li>
+				</ul>
+			</div>
+			<div className="sidebarMenu">
+				<h3 className="sidebarTitle">Notifications</h3>
+				<ul className="sidebarList">
+					<li className="sidebarListItem">
+						<MailOutline className="sidebarIcon" />
+						Mail
+					</li>
+					<li className="sidebarListItem">
+						<DynamicFeed className="sidebarIcon" />
+						Feedback
+					</li>
+					<li className="sidebarListItem">
+						<ChatBubbleOutline className="sidebarIcon" />
+						Message
+					</li>
+				</ul>
+			</div>
+			<div className="sidebarMenu">
+				<h3 className="sidebarTitle">Staffs</h3>
+				<ul className="sidebarList">
+					<li className="sidebarListItem">
+						<WorkOutline className="sidebarIcon" />
+						Manage
+					</li>
+					<li className="sidebarListItem">
+						<Timeline className="sidebarIcon" />
+						Analytics
+					</li>
+					<li className="sidebarListItem">
+						<ErrorOutline className="sidebarIcon" />
+						Reports
+					</li>
+				</ul>
+			</div>
+		</div>
 	</div>
   )
 }
